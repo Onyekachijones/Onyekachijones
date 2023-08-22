f104eb410dc8d956011cc03160a672566c0c2cca
Onyekachijones <142891950+Onyekachijones@users.noreply.github.com>
2023-08-22 11:16:30 +0100

    Introduction....

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Onyekachijones
+- 👀 I’m interested in collaborating & partnership.
+- 🌱 I’m currently learning how Tech recruitment and Talent Acquisition works
+- 💞️ I’m looking to collaborate on Recruitment and Talent solutions.
+- 📫 How to reach me via 
+
+<!---
+Onyekachijones/Onyekachijones is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
